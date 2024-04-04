c4ab830357d326573307e2d0d9e1ab05b1c42d70
Manuel Garcia Rodriguez <manuel.garcia.rodriguez.010@gmail.com>
2024-04-04 18:52:58 +0200

    Conexion/desconexion servidor SMTP y Imap

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,19 @@
 package main;
 
+import Client.EmailClientConnection;
+
 public class Main {
 
     public static void main(String[] args) {
+        EmailClientConnection EmailClientConnection = new EmailClientConnection();
         
+        EmailClientConnection.ConectionImap();
+        
+        EmailClientConnection.ConectionSMTP();
+        
+        EmailClientConnection.CloseConectionImap();
+        
+        EmailClientConnection.CloseConectionSMTP();
+
     }
-    
 }

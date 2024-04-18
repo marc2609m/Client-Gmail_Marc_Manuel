7e03bafcc6e41117cbed59e404de7adaabc6a826
Manuel Garcia Rodriguez <manuel.garcia.rodriguez.010@gmail.com>
2024-04-18 15:19:53 +0200

    Bandeja enviados boton mes

--- src/bandejas/BandejaEnviats.java
+++ src/bandejas/BandejaEnviats.java
@@ -7,28 +7,34 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import main.Login;
 
-public class BandejaEnviats extends JFrame{
+public class BandejaEnviats extends JFrame {
+
     private EmailClientConnection ecc;
+    private List<Mail> allMailsSended;
+    private int currentIndex = 0;
 
     public BandejaEnviats() {
         bandejaEnviats();
     }
-    
-    private void bandejaEnviats(){
+
+    private void bandejaEnviats() {
         setTitle("Enviats");
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+
         JMenuBar menuBar = new JMenuBar();
-        
+
         JMenu menuBandejas = new JMenu("Safates");
         JMenuItem BandejaPrincipalItem = new JMenuItem("Safata principal");
         JMenuItem EnviadoslItem = new JMenuItem("Enviats");
@@ -38,35 +44,41 @@ public class BandejaEnviats extends JFrame{
         menuBandejas.add(EnviadoslItem);
         menuBandejas.add(EsborranysItem);
         menuBandejas.add(CorreuBrossaItem);
-        
+
         JMenu menuFunciones = new JMenu("Funcions");
         JMenuItem EnviarMailItem = new JMenuItem("Enviar Mail");
         menuFunciones.add(EnviarMailItem);
-        
+
         JMenu menuLogout = new JMenu("Log out");
         JMenuItem LogoutItem = new JMenuItem("Log out");
         menuLogout.add(LogoutItem);
-        
+
         menuBar.add(menuBandejas);
         menuBar.add(menuFunciones);
         menuBar.add(menuLogout);
-        
+
         ecc = new EmailClientConnection();
-        
-        List<Mail> mails = ecc.ConseguirEnviados();
-        
-        Object[][] data = new Object[mails.size()][2];
 
-        for (int i = 0; i < mails.size(); i++) {
-            Mail m = mails.get(i);
-            data[i][0] = m.getRemitente();
-            data[i][1] = m.getAsunto();
-        }
+        loadInitialMails();
 
-        String[] columnNames = {"Emissor", "Assumpte"};
+        Object[][] data = new Object[allMailsSended.size()][2];
+        populateTableData(data);
+
+        String[] columnNames = {"Destinatari", "Assumpte"};
         JTable table = new JTable(data, columnNames);
         JScrollPane scrollPane = new JScrollPane(table);
-        
+
+        JButton moreButton = new JButton("Més");
+        moreButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                loadMoreMails();
+                Object[][] newData = new Object[allMailsSended.size()][2];
+                populateTableData(newData);
+                table.setModel(new DefaultTableModel(newData, columnNames));
+            }
+        });
+
         LogoutItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -77,7 +89,7 @@ public class BandejaEnviats extends JFrame{
                 setVisible(false);
             }
         });
-        
+
         EnviarMailItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -86,7 +98,7 @@ public class BandejaEnviats extends JFrame{
                 setVisible(false);
             }
         });
-        
+
         BandejaPrincipalItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -95,16 +107,7 @@ public class BandejaEnviats extends JFrame{
                 setVisible(false);
             }
         });
-        
-        BandejaPrincipalItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                BandejaPrincipal bp = new BandejaPrincipal();
-                bp.setVisible(true);
-                setVisible(false);
-            }
-        });
-        
+
         EsborranysItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -113,7 +116,7 @@ public class BandejaEnviats extends JFrame{
                 setVisible(false);
             }
         });
-        
+
         CorreuBrossaItem.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -122,9 +125,34 @@ public class BandejaEnviats extends JFrame{
                 setVisible(false);
             }
         });
-        
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(moreButton);
+
         getContentPane().add(scrollPane, BorderLayout.CENTER);
-        
+        getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
         setJMenuBar(menuBar);
+    }
+
+    private void loadInitialMails() {
+        // Cargar los últimos 10 correos enviados
+        allMailsSended = ecc.ConseguirEnviados(0, 9);
+        currentIndex = allMailsSended.size() - 1; // Actualizar el índice
+    }
+
+    private void loadMoreMails() {
+        int endIndex = currentIndex - 1; // El índice del correo anterior al último correo actual
+        int startIndex = Math.max(0, endIndex - 9); // El índice del correo anterior al primer correo actual
+        allMailsSended.addAll(0, ecc.ConseguirEnviados(startIndex, endIndex));
+        currentIndex = startIndex - 1; // Actualizar el índice
+    }
+
+    private void populateTableData(Object[][] data) {
+        for (int i = 0; i < allMailsSended.size(); i++) {
+            Mail m = allMailsSended.get(i);
+            data[i][0] = m.getRemitente();
+            data[i][1] = m.getAsunto();
+        }
     }
 }
